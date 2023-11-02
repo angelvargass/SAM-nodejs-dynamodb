a29d96982551cea681c3349fda09d6472b88549c
angelvargass <59036972+angelvargass@users.noreply.github.com>
2023-11-01 23:13:46 -0600

    removed find by message

--- src/functions/findMessages.mjs
+++ src/functions/findMessages.mjs
@@ -3,26 +3,13 @@ import { responseObject } from './utils.mjs';
 const tableName = process.env.TABLE_NAME
 
 export const lambdaHandler = async (event, context) => {
-    const messageString = event.queryStringParameters?.message || undefined;
-    const messages = await findMessages(messageString);
+    const messages = await findMessages();
     return responseObject(200, { messages: messages });
 };
 
-const findMessages = async (messageString) => {
+const findMessages = async () => {
     try {
         const dynamoClient = new DynamoDB.DocumentClient();
-        if (messageString) {
-            const params = {
-                TableName: tableName,
-                FilterExpression: 'message = :value',
-                ExpressionAttributeValues: {
-                    ':value': messageString.toLowerCase()
-                }
-            }
-            const response = await dynamoClient.scan(params).promise();
-            return response.Items;
-        }
-
         const response = await dynamoClient.scan({ TableName: tableName }).promise();
         return response.Items;
     } catch (error) {
